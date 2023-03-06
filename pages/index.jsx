e61eadc1684272f52bd36b8a29fb80ff7d4c80ba
alam-gir <info.alamgirhussain@gmail.com>
2023-03-06 22:10:08 +0600

    next package installed

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1>bismillah</h1>
+    </div>
+  )
+}
