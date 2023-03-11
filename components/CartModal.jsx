882ecd8070e5f5ab6d6d922142650a5cf89cc787
alam-gir <info.alamgirhussain@gmail.com>
2023-03-11 16:09:12 +0600

    cart designed.

---
+++ components/CartModal.jsx
@@ -0,0 +1,58 @@
+import { toggleCartState } from "@/utils/atom/cartRecoil";
+import React from "react";
+import { useRecoilState } from "recoil";
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import CartItem from "./CartItem";
+import CartPricing from "./CartPricing";
+
+const CartModal = () => {
+  const [isOpenCart, setOpenCart] = useRecoilState(toggleCartState);
+  const stopP = (event) => event.stopPropagation();
+  const handleExit = (e) => {
+    stopP(e);
+    setOpenCart(!isOpenCart);
+  };
+  return (
+    <>
+      {isOpenCart && (
+        // <div onClick={e => handleExit(e)} className="cart-modal-container">
+        <div onClick={(e) => stopP(e)} className="cart-modal-wrapper h-screen">
+          <div className="h-full">
+            {/* header  */}
+            <div className="flex justify-between h-12 items-center px-4 sm:p-2 shadow-[0_-2px_10px_rgba(0,0,0,0.20)]">
+              <h2 className="font-semibold text-gray-1700 capitalize">cart</h2>
+              <XCircleIcon
+                onClick={handleExit}
+                className="h-7 text-gray-700 hover:text-[#e50914]"
+              />
+            </div>
+
+            {/* body  */}
+            <div className="flex flex-col justify-between h-[calc(100vh-3rem)] p-4 sm:p-2">
+              {/* cart items  */}
+              <div className="max-h-[62%] overflow-y-scroll px-2 py-4 flex flex-col gap-6">
+                <div className="h-20">
+                  <CartItem />
+                </div>
+                <div className="h-20">
+                  <CartItem />
+                </div>
+                <div className="h-20">
+                  <CartItem />
+                </div>
+              </div>
+
+              {/* pricing section  */}
+              <div className="h-[38%] rounded-md shadow-[0_-2px_10px_rgba(0,0,0,0.1)] px-2 py-4">
+                <CartPricing />
+              </div>
+            </div>
+          </div>
+        </div>
+        // </div>
+      )}
+    </>
+  );
+};
+
+export default CartModal;
---
+++ components/CartPricing.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const CartPricing = () => {
+  return (
+    <div className='h-full'>
+        <div className='capitalize flex flex-col h-full justify-between'>
+            <div>
+                <h2 className='font-semibold text-gray-700'>price details</h2>
+            </div>
+            <div className=''>
+                <div className='grid grid-cols-2'><span>bag dicount</span> <span className='text-right'>$130.00</span></div>
+                <div className='grid grid-cols-2'><span>estimated text</span> <span className='text-right'>$130.00</span></div>
+                <div className='grid grid-cols-2'><span>delivery charge</span> <span className='text-right'>$130.00</span></div>
+                <div className='grid grid-cols-2'><span>total amount</span> <span className='text-right'>$130.00</span></div>
+            </div>
+            <div className='grid grid-cols-2'>
+                <h3>total amount</h3>
+                <h3 className='text-right text-[#FF4C4C]'>$150.00</h3>
+            </div>
+            <div className=''>
+                <button className='text-center w-full bg-[#FF4C4C] text-white capitalize font-semibold py-1 tracking-wide hover:brightness-90 rounded'>order now</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartPricing
