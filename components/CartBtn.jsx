431f5c67a18c3141a12863064f93c03af5de3458
alam-gir <info.alamgirhussain@gmail.com>
2023-03-13 14:23:41 +0600

    cart notification and showing empty cart and add to cart btn text dynamic.

--- components/CartBtn.jsx
+++ components/CartBtn.jsx
@@ -1,15 +1,26 @@
-import { toggleCartState } from '@/utils/atom/cartRecoil'
-import { ShoppingBagIcon } from '@heroicons/react/24/solid'
-import { useRecoilState } from 'recoil'
+import { toggleCartState } from "@/utils/atom/cartRecoil";
+import { cartState } from "@/utils/atom/cartState";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 const CartBtn = () => {
-    const [isOpenCart, setOpenCart] = useRecoilState(toggleCartState)
-    const handleClick = () => setOpenCart(!isOpenCart)
+  const [isOpenCart, setOpenCart] = useRecoilState(toggleCartState);
+  const cart = useRecoilValue(cartState);
+  const handleClick = () => setOpenCart(!isOpenCart);
   return (
-    <div>
-        <button onClick={handleClick}><ShoppingBagIcon className='h-8 w-8 bg-[#e50914] rounded-md p-1 text-white hover:brightness-75'/></button>
-    </div>
-  )
-}
+    <>
+      <div className="relative">
+        <button onClick={handleClick}>
+          <ShoppingBagIcon className="h-8 w-8 bg-[#227C70] rounded-md p-1 text-white hover:brightness-75" />
+        </button>
+        {cart?.products?.length > 0 && (
+          <span className="absolute z-10 bg-[#e50914] shadow-[0_-2px_10px_rgba(0,0,0,0.15)] translate-x-[-50%] translate-y-[-50%] h-6 w-6 rounded-full text-white text-center text-sm leading-6">
+            {cart?.products?.length}
+          </span>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default CartBtn
+export default CartBtn;
