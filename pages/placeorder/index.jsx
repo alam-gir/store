8c69a443f7f1c04cf21ca3451b8cc07ca27685a0
Ashraf_uddin <dev.fahim2021@gmail.com>
2023-03-14 19:52:46 +0600

    some edit from prettier

--- pages/placeorder/index.jsx
+++ pages/placeorder/index.jsx
@@ -1,11 +1,10 @@
-import { cartProductsIdState } from "@/utils/atom/cartProductsIdState";
-import { cartState } from "@/utils/atom/cartState";
+import {cartProductsIdState} from "@/utils/atom/cartProductsIdState";
+import {cartState} from "@/utils/atom/cartState";
 import React from "react";
-import { useRecoilState } from "recoil";
+import {useRecoilState} from "recoil";
 import CheckoutPage from "../../components/CheckoutPage";
 
 export default function Checkout() {
-  
   const [cartProductsId, setCartProductsId] =
     useRecoilState(cartProductsIdState);
   const [cart, setCart] = useRecoilState(cartState);
