bffaa3b5c739b9158f4ce03a7b71814fb5d6d566
alam-gir <info.alamgirhussain@gmail.com>
2023-03-16 10:01:53 +0600

    make dynamic price details list.

--- components/CartPricing.jsx
+++ components/CartPricing.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 
-const CartPricing = ({cart: {priceDetails}}) => {
+const CartPricing = ({cart: {priceDetails},isHeader, isTotalAmount, isBagDiscount, isEstimatedTax, isDeliveryCharge, isSubTotalAmount}) => {
   return (
     <div className="bg-white my-4 rounded-lg px-4 py-4 drop-shadow">
       <div className="capitalize flex flex-col gap-4">
         {/* Title Text */}
-        <h2 className="font-semibold text-black">price details</h2>
+        { isHeader && <h2 className="font-semibold text-black">price details</h2>}
 
         {/* Pricing List */}
         <ul className="totalPricingList">
-          <li>
+          {isTotalAmount && <li>
             <span>total amount</span>
             <span className="text-right">
               ${priceDetails?.totalAmount?.toFixed(2)}
             </span>
-          </li>
-          <li>
+          </li>}
+          { isBagDiscount && <li>
             <span>bag discount</span>
             <span className="text-right">
               ${priceDetails?.bagDiscount?.toFixed(2)}
             </span>
-          </li>
-          <li>
+          </li>}
+          { isEstimatedTax && <li>
             <span>estimated tax</span>{" "}
             <span className="text-right">
               ${priceDetails?.estimatedTax?.toFixed(2)}
             </span>
-          </li>
-          <li>
+          </li>}
+         {isDeliveryCharge && <li>
             <span>delivery charge</span>{" "}
             <span className="text-right">
               ${priceDetails?.deliveryCharge?.toFixed(2)}
             </span>
-          </li>
-          <li className="subTotalAmount">
+          </li>}
+          { isSubTotalAmount && <li className={`subTotalAmount ${isBagDiscount | isDeliveryCharge | isEstimatedTax | isHeader |isTotalAmount ? "pt-3 mt-3 border-t" : ''}`}>
             <h3>sub total</h3>
             <h3 className="text-red-500">
               ${priceDetails?.subTotal?.toFixed(2)}
             </h3>
-          </li>
+          </li>}
         </ul>
       </div>
     </div>
