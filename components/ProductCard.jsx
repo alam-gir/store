f7e1fbf5f9d28d338bf57d5ff88fb9015662ec06
Ashraf_uddin <dev.fahim2021@gmail.com>
2023-03-16 23:44:15 +0600

    Home page Design For Desktop View

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -11,23 +11,25 @@ export default function ProductCard({
   const productDefaultImg =
     "https://i.ibb.co/P9fVhj6/pngfind-com-lemon-tea-png-6661129.png";
 
+  const productCardBg = bgColor ? bgColor : "bg-white";
+
   return (
     <Link href={`/products/${_id}`}>
-      <div className={`product-card ${bgColor}`}>
-        <div className={`img-wrapper h-[8rem] w-auto object-contain`}>
+      <div className={`productCardBg ${productCardBg}`}>
+        <div className={`img-wrapper`}>
           <img
             src={images?.length > 0 ? images[0] : productDefaultImg}
             alt="productImg"
             loading="lazy"
-            className={`product-img h-full ${imgHeight}`}
+            className={`product-img ${imgHeight}`}
           />
         </div>
         <div className="product-details">
-          <h3 className={`product-name ${nameTextSize}`}>
+          <h3 className={`product-name`}>
             {name.slice(0, 10)}
             {weight && <span className="text-sm"> - {weight}</span>}
           </h3>
-          <strong className={`product-price ${priceTextSize}`}>${price}</strong>
+          <strong className={`product-price`}>${price}</strong>
         </div>
       </div>
     </Link>
