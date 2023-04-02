bc9a1ddb3cfa1cecd6bd885c5f754c0182de8111
alam-gir <info.alamgirhussain@gmail.com>
2023-04-02 07:27:53 +0600

    url change on open order modal added

--- components/dashboard/orders/NewOrdersLanding.jsx
+++ components/dashboard/orders/NewOrdersLanding.jsx
@@ -3,28 +3,69 @@ import useSWR, { useSWRConfig } from "swr";
 import { fetcher } from "@/lib/fetcher/fetcher";
 import { useRecoilState } from "recoil";
 import { crudOrderState } from "@/lib/atom/dashboard/crudOrder";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import LoaderSVG from "@/components/LoaderSVG";
+import { useRouter } from "next/router";
+import ReactModal from "react-modal";
+import ViewOrders from "./ViewOrders";
 
 const NewOrdersLanding = () => {
+  // * states
   const [crudOrderAction, setCrudOrderAction] = useRecoilState(crudOrderState);
+  const [queryOrderId, setOrderId] = useState(null);
+  const router = useRouter();
 
   const { data, error, isLoading } = useSWR(
     "/api/db/orders/placeorder",
     fetcher
   );
-  console.log({data, isLoading})
   //for revalidating / refetching...
   const { mutate } = useSWRConfig();
 
+  // * arrow functions
+  const openOrderModal = (id) => {
+    //stop body scroll 
+    document.body.style.overflow = 'hidden'
+    //set new order id
+    router.push(
+        `/dashboard/orders/new?orderId=${id}`,
+        undefined,
+        {
+          scroll: false,
+        }
+    )
+  }
+  const closeOrderModal = () => {
+    //start body scroll 
+    document.body.style.overflow = 'unset'
+    //set Order id to null
+    setOrderId(null)
+    // back to previous page on router for close modal
+    router.push(
+        `/dashboard/orders/new`,
+        undefined,
+        {
+          scroll: false,
+        }
+    )
+  }
+
+  // * effects
   useEffect(() => {
     mutate("/api/db/orders/placeorder");
   }, [crudOrderAction]);
+
+  useEffect(() => {
+    setOrderId(router.query.orderId);
+  }, [router.query.orderId]);
+
+  // * functions
 
   const orderCards = data?.newOrders?.map((order, index) => {
     return (
       <OrderCard
         key={index}
+        typeText="ordered at"
         date={order.orderedAt.toString()}
         id={order._id.toString()}
         name={order.customer.fullName.toString()}
@@ -32,13 +73,22 @@ const NewOrdersLanding = () => {
         phone={order.customer.mobileNum.toString()}
         city={order.customer.cityName.toString()}
         division={order.customer.userDistrict.toString()}
+        clickHandler={() => openOrderModal(order.orderId)}
       />
     );
   });
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-2 md:flex-row md:flex-wrap md:p-4">
-      {isLoading ? <LoaderSVG color={"fill-gray-400"} /> : orderCards}
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center gap-2 p-2 md:flex-row md:flex-wrap md:p-4">
+        {isLoading ? <LoaderSVG color={"fill-gray-400"} /> : orderCards}
+      </div>
+      <ReactModal
+        isOpen={!!queryOrderId}
+        onRequestClose={() => closeOrderModal()}
+      >
+        <ViewOrders order={queryOrderId} />
+      </ReactModal>
+    </>
   );
 };
 
---
+++ components/dashboard/orders/ViewOrders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewOrders = ({order}) => {
+  return (
+    <div>{order}</div>
+  )
+}
+
+export default ViewOrders
